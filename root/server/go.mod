dbb6d0059f19ee82537b787b715c4596805a28e1
Roman <m3f3dr0nbristleback@gmail.com>
2026-01-07 22:58:15 +0300

    test

--- root/server/go.mod
+++ root/server/go.mod
@@ -4,6 +4,7 @@ go 1.25.5
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/gorilla/websocket v1.5.3
 	github.com/jackc/pgx/v5 v5.8.0
 	github.com/joho/godotenv v1.5.1
 )
--- root/server/main.go
+++ root/server/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
-
-	"github.com/jackc/pgx/v5"
-	"github.com/joho/godotenv"
-
 	"main-crypto/pkg/bot"
 	"main-crypto/pkg/db"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 var dbConn *pgx.Conn
+
+var lastPrice string = "loading..."
+var mu sync.RWMutex
 
 func main() {
 	err := godotenv.Load()
@@ -23,5 +29,75 @@ func main() {
 
 	defer dbConn.Close(context.Background())
 
-	bot.Conn(conn)
+	go bot.Conn(conn)
+
+	go wsToBtc()
+
+	go runHTTP()
+
+	select {}
+
+}
+
+func runHTTP() {
+	http.HandleFunc("/price", func(w http.ResponseWriter, r *http.Request) {
+		mu.RLock()
+		price := lastPrice
+		mu.RUnlock()
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		json.NewEncoder(w).Encode(map[string]string{"btcPrice": price})
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+type TickerMsg struct {
+	Topic string `json:"topic"`
+	Data  struct {
+		LastPrice string `json:"lastPrice"`
+	} `json:"data"`
+}
+
+func wsToBtc() {
+	url := "wss://stream.bybit.com/v5/public/spot"
+
+	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		log.Println("Conn err:", err)
+	}
+
+	defer conn.Close()
+
+	sub := map[string]interface{}{
+		"op": "subscribe",
+		"args": []string{
+			"tickers.BTCUSDC",
+		},
+	}
+
+	msg, _ := json.Marshal(sub)
+	conn.WriteMessage(websocket.TextMessage, msg)
+
+	for {
+		_, msg, err := conn.ReadMessage()
+
+		if err != nil {
+			log.Println("Conn read:", err)
+			continue
+		}
+
+		var ticker TickerMsg
+
+		if err := json.Unmarshal(msg, &ticker); err != nil {
+			continue
+		}
+
+		if ticker.Data.LastPrice != "" {
+			mu.Lock()
+			lastPrice = ticker.Data.LastPrice
+			mu.Unlock()
+		}
+	}
 }
