96c7e3c1a82715756bbf68fc4b24da09184c142c
Roman <m3f3dr0nbristleback@gmail.com>
2026-01-07 05:43:31 +0300

    update interface soon

--- root/client/src/App.tsx
+++ root/client/src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
+import Snowfall from "react-snowfall";
+
 function App() {
+  const [color] = useState({
+    dark: "#111111",
+    accent: "#222222"
+  });
+
+  const [colorText] = useState("#FFFFFF")
 
   return (
     <>
-      <div>
-        Hello, World!
+      <div style={{background: `linear-gradient(120deg, ${color.dark}, ${color.accent})`, height: '100vh', padding: '20px'}}>
+        <h1 style={{ color: colorText, textAlign: "center"}}>Soon</h1>
+        <Snowfall/>
       </div>
     </>
       
